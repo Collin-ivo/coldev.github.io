acc539344fe7b29988c4119fd7c6549b699b7c3f
Nikolay Ivanov <n_13@bk.ru>
2019-10-09 21:49:09 +0300

    первые сайты

---
+++ catenergy/js/module.js
@@ -0,0 +1,12 @@
+var module = (function () {
+  'use strict';
+
+  const functionModule = () => {
+    console.log('fkkffhhy');
+  };
+
+  return functionModule;
+
+}());
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9kdWxlLmpzIiwic291cmNlcyI6WyJzcmMvanMvbW9kdWxlLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGZ1bmN0aW9uTW9kdWxlID0gKCkgPT4ge1xuICBjb25zb2xlLmxvZygnZmtrZmZoaHknKTtcbn07XG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uTW9kdWxlO1xuIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztFQUFBLE1BQU0sY0FBYyxHQUFHLE1BQU07RUFDN0IsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFVBQVUsQ0FBQyxDQUFDO0VBQzFCLENBQUMsQ0FBQzs7Ozs7Ozs7In0=
